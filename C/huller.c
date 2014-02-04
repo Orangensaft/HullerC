f0f0bf33faa70ea8e073c2355285045de0587117
r0binf-92 <robin.franzke@hotmail.de>
2014-02-04 10:35:18 +0100

    Comments
    Parameter in den Kommentaren angegeben…

--- C/huller.c
+++ C/huller.c
@@ -86,13 +86,13 @@ int main(int argc, char **argv){
     }  
     if(argc==4){
             if(strcmp(argv[1],"learn")==0){
-                learn(argv[2],atoi(argv[3]));
+                learn(argv[2],atoi(argv[3])); // argv[2] = SVM-File ; argv[3] = Dimension
                 exit(EXIT_SUCCESS); 
             }
     }
     if(argc==5){
             if(strcmp(argv[1],"classify")==0){
-                classify(argv[2],argv[3],atoi(argv[4]));
+                classify(argv[2],argv[3],atoi(argv[4])); // argv[2] = SVM-File ; argv[3] = unser Modell ; argv[4] = Dimension
                 exit(EXIT_SUCCESS);
             }
     }
